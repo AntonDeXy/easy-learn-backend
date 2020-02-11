334626669aad6df798ffb1fb1b9ee56c497a9ea3
AntonDeXy <podolyananton@gmail.com>
2020-02-12 00:29:30 +0200

    db uri from env

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var ObjectID = require('mongodb').ObjectId
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose') 
-const url = 'mongodb+srv://podolyananton:bslukY3JgdGVRrDv@cluster0-s6ujg.mongodb.net/test?retryWrites=true&w=majority'
+const url = process.env.MONGODB_URI || 'mongodb://antonDexy:1gdy54ff@ds163517.mlab.com:63517/heroku_13pcnz63'
+// const url = 'mongodb+srv://podolyananton:bslukY3JgdGVRrDv@cluster0-s6ujg.mongodb.net/test?retryWrites=true&w=majority'
 const articleSchema = require('./models/itemsSchema')
 const cookieParser = require('cookie-parser')
 const articlesControllers = require('./controllers/category')
